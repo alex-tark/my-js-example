dae84c47705d36422b72342cc7e32e082e3d7926
Alexander Zinin <nazicrim@yandex.ru>
2018-02-13 15:02:29 +0300

    Build typescript auth module

--- server/api/auth/controller/auth-controller.js
+++ server/api/auth/controller/auth-controller.js
@@ -71,6 +71,25 @@ var AuthController = /** @class */ (function () {
         })
             .catch(function (error) { return res.status(400).json(error); });
     };
+    /**
+     * @api{POST} /auth/token Access_token
+     * @apiVersion 0.0.1
+     * @apiName  Token
+     * @apiGroup OAuth
+     *
+     * @apiParam {String} access_token User unique access token
+     *
+     * @apiSuccess{Boolean} success       Final request flag
+     * @apiSuccess{String}  message       Server request message
+     * @apiSuccess{String}  access_token  OAuth grand access token
+     *
+     * @apiSuccessExample Success example:
+     * {
+     *    success: true,
+     *    message: "Token is available",
+     *    access_token: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+     * }
+     */
     AuthController.tokenStatus = function (req, res) {
         var _token = req.body.access_token;
         token_dao_1.default["checkRelevance"](_token)
